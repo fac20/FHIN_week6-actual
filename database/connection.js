738f6f681f2f173368afbbc0d58c483d4819d0c4
Terry Abdullah <glenroyterence@gmail.com>
2020-08-20 14:08:24 +0100

    Link style sheet
    Co-authored-by: Jihyun-Jang <jhjang742@gmail.com>

--- database/connection.js
+++ database/connection.js
@@ -10,15 +10,8 @@ if (!dataBaseURL) {
   };
 
 const options = {
-<<<<<<< HEAD
-    connectionString: dataBaseURL
-||||||| 9061910
-    connectionString: dataBaseURL,
-    ssl: { rejectUnauthorized: false },
-=======
     connectionString: dataBaseURL,
     // ssl: { rejectUnauthorized: false },
->>>>>>> master
   };
 const db = new pg.Pool(options);
 
